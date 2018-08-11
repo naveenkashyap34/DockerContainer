1fa583ebc6bb71450e41dc075449ca338a0afc2b
Naveen <naveen.k343@gmail.com>
2018-08-12 01:45:22 +0530

    My First Commit

---
+++ DockerContainer/Models/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DockerContainer.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name ="First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LasttName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password doesn't match.")]
+        [Display(Name = "Confirm Password")]
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
